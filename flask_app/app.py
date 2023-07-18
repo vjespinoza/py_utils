468a4bdf21554a2c879872eca4b8c95d9656edf0
Victor Espinoza <vjespinoza84@gmail.com>
2023-07-17 19:42:50 -0500

    Implement dropzone

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,8 +1,13 @@
+import os
 from typing import Any
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_dropzone import Dropzone
+
+from app_functionalities.image_utilities import remove_background
 
 app = Flask(__name__)
+dropzone = Dropzone(app)
 
 
 @app.route('/')
@@ -13,6 +18,21 @@ def home() -> Any:
 @app.route('/remove_bg')
 def remove_bg() -> Any:
     return render_template('remove_bg.html')
+
+
+@app.route('/uploads', methods=['GET', 'POST'])
+def uploads() -> Any:
+    if request.method == 'POST':
+        f = request.files.get('file')
+        f.save(os.path.join('../resources/img', f.filename))
+
+    return render_template('uploads.html')
+
+
+@app.route('/process_bg_removal')
+def process_bg_removal() -> Any:
+    remove_background()
+    return ""
 
 
 if __name__ == '__main__':
